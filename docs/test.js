c1ba719a0536910ab04ca4839904fc28a45c76ab
sanxy.zhang <sanxy.zhang@Qunar.com>
2018-09-10 16:24:54 +0800

    haha

--- docs/test.js
+++ docs/test.js
@@ -1,21 +1,3 @@
-function isPrimeNum(num){
-  for (var i = 2; i < num; i++) {
-  if (num%i==0){
-  return false;
-  }
-  };
-  return true;
-  }
-function sumNum(n){
-    var first=1;
-    var sum=0;
-    for(var i=1;i<n;i++){
-      if(isPrimeNum(first)){
-        sum+=first;
-      }
-      first=first+2;
-    }
-    return sum;
-}
-
-console.log(sumNum(4))
+var n=readInt();
+var arr=read_line();
+console.log(n+arr);
